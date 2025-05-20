7e4dd442f809180bec727ea157733ef400c699f6
Babak Taremi <babak.taremi.dev@gmail.com>
2025-05-20 12:33:39 +0330

    updated Aspire App Host and update Worker class for migration handling

--- MiniApi.MigrationService/Worker.cs
+++ MiniApi.MigrationService/Worker.cs
@@ -3,24 +3,32 @@ using MiniApi.Shared.Database;
 
 namespace MiniApi.MigrationService;
 
-public class Worker(ILogger<Worker> logger, IServiceScopeFactory serviceScopeFactory)
+public class Worker(ILogger<Worker> logger
+    , IServiceScopeFactory serviceScopeFactory
+    , IHostApplicationLifetime hostApplicationLifetime)
     : BackgroundService
 {
-   
-
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
+        await Task.Yield();
         logger.LogWarning("Applying Migrations");
         using var scope= serviceScopeFactory.CreateScope();
         var db = scope.ServiceProvider.GetRequiredService<MiniApiDbContext>();
 
-        var strategy = db.Database.CreateExecutionStrategy();
-        
-       await strategy.ExecuteAsync(async () =>
+        try
         {
-            await db.Database.MigrateAsync(stoppingToken);
-        });
+            var strategy = db.Database.CreateExecutionStrategy();
+        
+            await strategy.ExecuteAsync(async () =>
+            {
+                await db.Database.MigrateAsync(stoppingToken);
+            });
        
-       logger.LogWarning("Migrations Applied");
+            logger.LogWarning("Migrations Applied");
+        }
+        finally
+        {
+            hostApplicationLifetime.StopApplication();
+        }
     }
 }
