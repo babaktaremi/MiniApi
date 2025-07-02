c1c37ee8c5addca58373a5f2b3fc5e0241cd21ee
Babak Taremi <babak.taremi.dev@gmail.com>
2025-07-02 16:57:23 +0330

    Added integration testing using Aspire

---
+++ MiniApi.IntegrationTests/ApiTests.cs
@@ -0,0 +1,28 @@
+﻿using System.Net;
+using System.Net.Http.Json;
+using Aspire.Hosting.Testing;
+
+namespace MiniApi.IntegrationTests;
+
+public class ApiTests
+{
+    [Fact]
+    public async Task Create_Order_Should_Return_Ok()
+    {
+        var appHost = await DistributedApplicationTestingBuilder.CreateAsync<Projects.MiniApi_Hosting>();
+
+        var app = await appHost.BuildAsync();
+
+        await app.ResourceNotifications.WaitForResourceHealthyAsync("miniApi");
+
+        var client =  app.CreateHttpClient("miniApi");
+
+        var result = await client.PostAsJsonAsync("/order", new
+        {
+            orderName = "TestOrder"
+        });
+        
+        Assert.Equal(HttpStatusCode.Created,result.StatusCode);
+        
+    }
+}
